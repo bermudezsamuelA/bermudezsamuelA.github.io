d64b6658dfa9627ff3ca0a63704b03b7f83348b4
Samuel Bermudez <sam93quimica14@gmail.com>
2022-01-28 20:25:49 -0400

    main: carousel

--- scripts/components/Knowlegde.js
+++ scripts/components/Knowlegde.js
@@ -2,62 +2,33 @@ const KnowLedge={
     template:`
     <article id="knowledge">
         <section id="knowledgeBars">
-            <div>
-                <label for="file">File progress:</label>
-                <progress id="file" max="100" value="70"> 70% </progress>
-            </div>
-            
-            <div>
-                <label for="file">File progress:</label>
-                <progress id="file" max="100" value="70"> 70% </progress>
+            <div class="progressSection"  v-for="key in ratesKeys">
+                <label for="rate" class="labelProgress">{{key}}: </label>
+                <progress id="file" max="100" v-bind:value="rates[key]"> {{rates[key]}}% </progress>
             </div>
         </section>
         <section id='carousel' class="carousel" aria-label="Gallery">
             <ol class="carousel__viewport">
-                <li id="carousel__slide1"
-                    tabindex="0"
-                    class="carousel__slide">
-                    <div class="carousel__snapper">
-                        <a href="#carousel__slide4"
-                        class="carousel__prev">Go to last slide</a>
-                            <input class="buttonCourse" type="button" value="Click me">
-                        <a href="#carousel__slide2"
-                            class="carousel__next">Go to next slide</a>
-                    </div>
-                </li>
-                <li id="carousel__slide2"
-                    tabindex="0"
-                    class="carousel__slide">
-                    <div class="carousel__snapper">
-                        <a href="#carousel__slide1"
-                        class="carousel__prev">Go to previous slide</a>
-                            <input class="buttonCourse" type="button" value="Click me">
-                        <a href="#carousel__slide3"
-                        class="carousel__next">Go to next slide</a>
-                    </div>
-                </li>
-                <li id="carousel__slide3"
-                    tabindex="0"
-                    class="carousel__slide">
-                    <div class="carousel__snapper">
-                        <a href="#carousel__slide2"
-                        class="carousel__prev">Go to previous slide</a>
-                            <input class="buttonCourse" type="button" value="Click me">
-                        <a href="#carousel__slide4"
-                        class="carousel__next">Go to next slide</a>
-                    </div>
-                </li>
-                <li id="carousel__slide4"
-                    tabindex="0"
-                    class="carousel__slide">
-                    <div class="carousel__snapper">
-                        <a href="#carousel__slide3"
-                        class="carousel__prev">Go to previous slide</a>
-                            <input class="buttonCourse" type="button" value="Click me">
-                        <a href="#carousel__slide1"
-                        class="carousel__next">Go to first slide</a>
-                    </div>
-                </li>
+                    <li  
+                        v-for="(button,index) in buttonTitles"
+                        :id="button._id"
+                        :key="button._id"
+                        tabindex="0"
+                        class="carousel__slide">
+                        <div class="carousel__snapper">
+                            <a 
+                            v-bind:href="button.before"
+                            class="carousel__prev">
+                                Go to last slide
+                            </a>
+                                <input class="buttonCourse" type="button" v-bind:value="button.title">
+                            <a 
+                            v-bind:href="button.after"
+                            class="carousel__next">
+                                Go to next slide
+                            </a>
+                        </div>
+                    </li>
             </ol>
         </section>
     </article>
@@ -65,12 +36,11 @@ const KnowLedge={
     components:['Modalcourses'],
     data(){
         return{
-            name: personal.name,
-            information: personal.information,
-            informationKeys: Object.keys(personal.information),
-            icons: socialMedia,
+            rates: ratesKnowlege,
+            ratesKeys: Object.keys(ratesKnowlege),
+            buttonTitles: buttonTitles,
         }
-    },
+    },  
     methods:{
 
     },
